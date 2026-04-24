f4a943e115557b15955dd4416a5fb5284f117407
HenrBmp <henriquerodr20@gmail.com>
2026-04-24 16:40:24 -0300

    eslint adicionado

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import js from '@eslint/js';
+import globals, { commonjs } from 'globals';
+import json from '@eslint/json';
+import markdown from '@eslint/markdown';
+import css from '@eslint/css';
+import { defineConfig } from 'eslint/config';
+import { version } from 'react';
+
+export default defineConfig([
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    },
+    {
+        files: ['**/*.json'],
+        plugins: { json },
+        language: 'json/json',
+        extends: ['json/recommended'],
+    },
+    {
+        files: ['**/*.jsonc'],
+        plugins: { json },
+        language: 'json/jsonc',
+        extends: ['json/recommended'],
+    },
+    {
+        files: ['**/*.md'],
+        plugins: { markdown },
+        language: 'markdown/gfm',
+        extends: ['markdown/recommended'],
+    },
+    {
+        files: ['**/*.css'],
+        plugins: { css },
+        language: 'css/css',
+        extends: ['css/recommended'],
+    },
+    {
+        rules: {
+            indent: ['warn', 4],
+            semi: ['error', 'always'],
+        }
+    },
+    {
+        extends: [
+            'google',
+            'prettier'
+        ]
+    }
+]);
