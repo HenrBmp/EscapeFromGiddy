b45e0e293af7ff1714a08b6f9e8b57e857da63a1
HenrBmp <henriquerodr20@gmail.com>
2026-04-25 19:03:41 -0300

    nome do banco de dados movido para o .env

--- database.js
+++ database.js
@@ -1,14 +1,13 @@
-import { createPool } from "mysql2/promise";
-import { loadEnvFile } from "node:process";
+import { createPool } from 'mysql2/promise';
+import { loadEnvFile } from 'node:process';
 
-loadEnvFile()
+loadEnvFile();
 
 const mysql = await createPool({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSW,
     connectionLimit: 10,
-    database: "",
-    
-})
+    database: process.env.DATABASE,
+});
 
 export default mysql;
