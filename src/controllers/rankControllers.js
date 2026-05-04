b87516afaa4cba8d6d5319995884b4159610b908
HenrBmp <henriquerodr20@gmail.com>
2026-05-04 16:21:40 -0300

    implementei obterRecordeJogador

--- src/controllers/rankControllers.js
+++ src/controllers/rankControllers.js
@@ -1,22 +1,60 @@
 import mysql from '../../database.js';
-import { StatusCodes } from "http-status-codes";
+import StatusHttp from '../libs/HttpStatus.js';
+
+const jsonHeader = new Headers({
+    'Content-Type': 'application/json; charset=utf-8',
+});
+
 export default class RankControllers {
     /**
-     * Envia os jogadores e seu recorde de pontuação em ordem decrescente.
+     * Envia ao cliente os jogadores e seu recorde de pontuação em ordem decrescente.
      * @param {import("express").Request} req Requisição
      * @param {import("express").Response} res Resposta
      */
-    static async getRanking(req, res) {
-        const [result] = await mysql.query(
-            `SELECT nome, maior_pontuacao FROM jogador ORDER BY maior_pontuacao DESC;`,
-        );
-        res.setHeader('content-type', 'application/json; charset=utf-8').status(StatusCodes.OK).send(result);
+    static async obterRanking(req, res) {
+        res.setHeaders(jsonHeader);
+        try {
+            const [result] = await mysql.query(
+                `SELECT nome, maior_pontuacao FROM jogador ORDER BY maior_pontuacao DESC;`,
+            );
+            res.status(StatusHttp.OK).json(result);
+        } catch (error) {
+            res.status(StatusHttp.ERRO_INTERNO_DO_SERVIDOR).json({
+                message: 'Houve um erro na consulta ao banco de dados.',
+                error,
+            });
+        }
     }
 
     /**
-     * Atualiza o recorde de pontuação de um jogador.
+     *
      * @param {import("express").Request} req Requisição
      * @param {import("express").Response} res Resposta
      */
-    static insertRecord(req, res) {}
+    static async atualizarRecorde(req, res) {}
+
+    /**
+     * Envia aio cliente o recorde do jogador cujo nome foi recebido pelo corpo da requisição.
+     * Criara o jogador no banco de dados caso o mesmo nao exista, consequentemente enviando 0 como recorde.
+     * @param {import("express").Request} req Requisição
+     * @param {import("express").Response} res Resposta
+     */
+    static async obterRecordeJogador(req, res) {
+        const { nomeJogador } = req.body;
+        try {
+            const [result] = await mysql.execute(
+                'INSERT IGNORE INTO jogador VALUES (?, null);' +
+                'SELECT maior_pontuacao FROM jogador WHERE nome = ?;',
+                [nomeJogador, nomeJogador],
+            );
+            res.setHeader('content-type', 'plain/text; charset=utf8')
+                .status(StatusHttp.OK)
+                .send(result[0].maior_pontuacao.toString());
+        } catch (error) {
+            res.setHeader(jsonHeader).status(StatusHttp.ERRO_INTERNO_DO_SERVIDOR).json({
+                message: 'Houve um erro na consulta ao banco de dados.',
+                error,
+            });
+        }
+    }
 }
