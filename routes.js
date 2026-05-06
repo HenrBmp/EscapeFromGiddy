f2027121ff627fc64c4b313797da797dac8db45d
HenrBmp <henriquerodr20@gmail.com>
2026-05-05 23:01:26 -0300

    mover funcao getPage para arquivo separado

--- routes.js
+++ routes.js
@@ -2,22 +2,12 @@ import express from 'express';
 import { join } from 'path';
 import RankControllers from './src/controllers/rankControllers.js';
 import ValidacaoCorpoRequisicao from './src/middlewares/validacaoCorpoRequisicao.js';
+import getPage from './src/libs/getPage.js';
 
 const router = express.Router();
 
-/**
- * Obtem o caminho absoluto para uma página html em public/pages com base no caminho relativo a 'routes.js'.
- * @param {string} fileName Nome do arquivo a ser procurado (não é necessário a extensão).
- * @returns Caminho absoluto para o arquivo.
- */
-function getPage(fileName) {
-    return join(import.meta.dirname, 'public', 'pages', fileName.replace('.html', '') + '.html');
-}
-
 // =======================Pagina Inicial=======================
-router.get('/', (req, res) => {
-    res.sendFile(getPage('index'));
-});
+router.get('/', (req, res) => res.sendFile(getPage('index')));
 
 // =======================Ranking=======================
 router.get('/rank', ValidacaoCorpoRequisicao.verificarCorpo_e_Nome, RankControllers.obterRanking);
---
+++ src/libs/getPage.js
@@ -0,0 +1,10 @@
+import { join } from 'node:path';
+
+/**
+ * Obtem o caminho absoluto para uma página html em public/pages com base no caminho relativo a 'routes.js'.
+ * @param {string} fileName Nome do arquivo a ser procurado (não é necessário a extensão).
+ * @returns Caminho absoluto para o arquivo.
+ */
+export default function getPage(fileName) {
+    return join(import.meta.dirname, 'public', 'pages', fileName.replace('.html', '') + '.html');
+}
