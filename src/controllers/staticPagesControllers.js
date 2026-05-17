4484414db4d6d9635429d3f693484bafff30dee6
HenrBmp <henriquerodr20@gmail.com>
2026-05-16 23:56:39 -0300

    usar lib getPage

--- src/controllers/staticPagesControllers.js
+++ src/controllers/staticPagesControllers.js
@@ -1,34 +1,19 @@
-import { join } from 'node:path';
-export default class StaticPagesControllers {
-    /**
-     * Obtem o caminho absoluto para uma página html em public/pages com base no caminho relativo a 'routes.js'.
-     * @param {string} fileName Nome do arquivo a ser procurado (não é necessário a extensão).
-     * @returns Caminho absoluto para o arquivo.
-     */
-    getPage(fileName) {
-        return join(
-            import.meta.dirname,
-            '..',
-            '..',
-            'public',
-            'pages',
-            fileName.replace('.html', '') + '.html',
-        );
-    }
+import getPage from '../libs/getPage.js';
 
+export default class StaticPagesControllers {
     static htmlHeader = new Headers({
         'content-type': 'text/html; charset=utf-8',
     });
 
     static homePage(req, res) {
-        res.setHeaders(this.htmlHeader).status(200).sendFile(this.getPage('index'));
+        res.setHeaders(this.htmlHeader).status(200).sendFile(getPage('index'));
     }
 
     static rankPage(req, res) {
-        res.setHeaders(this.htmlHeader).status(200).sendFile(this.sendFile('rank'));
+        res.setHeaders(this.htmlHeader).status(200).sendFile(getPage('rank'));
     }
 
     static gamePage(req, res) {
-        res.setHeaders(this.htmlHeader).status(200).sendFile(this.sendFile('game'));
+        res.setHeaders(this.htmlHeader).status(200).sendFile(getPage('game'));
     }
 }
