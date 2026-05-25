17bb30587b7e48a3110d6094eae821998a00b13e
HenrBmp <henriquerodr20@gmail.com>
2026-05-25 17:03:33 -0300

    adicionar background na pagina game

--- public/scripts/home/popUpNomeJogador.js
+++ public/scripts/home/popUpNomeJogador.js
@@ -5,16 +5,16 @@ export default function popUpNomeJogador() {
         const popUp = document.importNode(template.content, true);
         document.body.append(popUp);
 
-        const submitButton = document.querySelector('form#popUp button');
+        const submitButton = document.querySelector('#popUp button');
         submitButton.addEventListener('click', async (event) => {
             event.preventDefault();
 
-            const nomeJogador = document.querySelector('form#popUp #inputNomeJogador').value;
+            const nomeJogador = document.querySelector('#popUp #inputNomeJogador').value;
 
             if (!nomeJogadorEhValido(nomeJogador))
                 window.alert('Seu nome deve ter no mínimo 3 caracteres e no máximo 30.');
 
-            document.querySelector('form#popUp').remove();
+            document.querySelector('#popUp').remove();
             localStorage.setItem('nomeJogador', nomeJogador);
 
             resolve(nomeJogador);
